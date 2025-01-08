9bbd0b38f6be7c19c775ccb194444806403f5da2
simonjisu <simonjisu@gmail.com>
2025-01-09 01:32:42 +0900

    update

--- run_evaluation.py
+++ run_evaluation.py
@@ -449,7 +449,8 @@ if __name__ == '__main__':
         pd.DataFrame(df).to_csv(eval_path / f'{args.ds}_{args.type}.csv', index=False)
 
     elif args.task == 'valid_bo':
-        filter_db_ids = ['bike_share_1', 'language_corpus', 'donor', 'menu', 'movie_platform']
+        filter_db_ids = ['bike_share_1', 'language_corpus', 'donor', 'menu', 
+                         'movie_platform', 'talkingdata', 'authors']
         # valid_bo
         paths = sorted(list(prediction_path.glob(f'{args.ds}_{args.type}_*.json')))
         paths = [p for p in paths if p.stem.split('_', 2)[-1].split('-')[0] not in filter_db_ids]
